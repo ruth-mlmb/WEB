89621f50a298b2c9bb1b1e7c6430ec1519a2f3ec
Slack0ff <122684258+Slack0ff@users.noreply.github.com>
2026-03-31 01:11:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,28 @@ import SosValides  from "./SOS_validés";
 import SosExpires  from "./SOS_expirés";
 import ValiderSOS  from "./ValiderSOS";
 import AjoutSos from "./ajout_SOS";
+import Accueil from "./Fct_Accueil";
+import { Profil, Liste, BDE, LoginUser, LoginCDP, LoginBDE } from "./Fct_login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/"            element={<PageAccueil />} />
+        <Route path="/" element={<Accueil />} />
+        <Route path="/home" element={<PageAccueil />} />
         <Route path="/valider/:id" element={<ValiderSOS />} />
-        <Route path="/valides"     element={<SosValides />} />
-        <Route path="/expires"     element={<SosExpires />} />
-        <Route path="/ajout"     element={<AjoutSos />} />
+        <Route path="/valides" element={<SosValides />} />
+        <Route path="/expires" element={<SosExpires />} />
+        <Route path="/ajout" element={<AjoutSos />} />
+
+        <Route path="/login_user" element={<LoginUser />} />
+        <Route path="/login_user/profil" element={<Profil />} />
+
+        <Route path="/login_cdp" element={<LoginCDP />} />
+        <Route path="/login_cdp/liste" element={<Liste />} />
+
+        <Route path="/login_bde" element={<LoginBDE />} />
+        <Route path="/login_bde/bde" element={<BDE />} />
       </Routes>
     </Router>
   );
